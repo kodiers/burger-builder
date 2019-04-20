6d6f451c85664d6ae63e77df566eab5a564a562d
Victor Yamchinov <kodiers@gmail.com>
2019-04-20 05:59:03 +0300

    added contactData

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -12,6 +14,15 @@ class Checkout extends Component {
       bacon: 1
     }
   };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ingredients: ingredients});
+  }
 
   checkoutCancelHandler = () => {
     this.props.history.goBack();
@@ -27,7 +38,9 @@ class Checkout extends Component {
         <CheckoutSummary
           ingredients={this.state.ingredients}
           checkoutCancel={this.checkoutCancelHandler}
-          checkoutContinued={this.checkoutContinuedHandler}/>
+          checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route path={this.props.match.path + '/contact-data'} component={ContactData}/>
       </div>
     );
   }
